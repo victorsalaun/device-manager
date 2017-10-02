4f203b0765d8c68ae47b001904015eeeb9a0ba6d
victorsalaun <victor.salaun@gmail.com>
2017-10-02 15:04:33 +0200

    add admin and api page to README.md + add Device Model to admin

--- README.md
+++ README.md
@@ -9,12 +9,20 @@ Very simple Django based app using  Django Rest Framework
 
 ## Installation
 
-    pip install -r requirements
+    pip install -r requirements.txt
 
 ## Init
 
     python manage.py migrate
 
+In order to create admin user:
+
+    python manager.py createsuperuser
+
 ## Launch
 
     python manage.py runserver
+    
+[Admin page](http://127.0.0.1:8000/admin/)
+
+[Api page](http://127.0.0.1:8000/api/devices/)
--- devices/views.py
+++ devices/views.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 
 from devices.models import Device
 from devices.serializers import DeviceSerializer
 
 
-class DeviceListCreateAPIView(ListCreateAPIView):
+class DeviceListCreateAPIView(ListAPIView):
     serializer_class = DeviceSerializer
 
     def get_queryset(self):
